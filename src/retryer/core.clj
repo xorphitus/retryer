3363b5b82806bee5ab593d9b79891fc93f8746a8
xorphitus <xorphitus@gmail.com>
2017-03-29 23:49:36 +0900

    リトライ時にsleepするようにした

--- src/retryer/core.clj
+++ src/retryer/core.clj
@@ -4,11 +4,14 @@
   "retry a given function"
   [f times execption]
   `(->> (take ~times (iterate inc 0))
-       (map
-        (fn [n#] (try
-                  (~f n#)
-                  (catch ~execption e#
-                    nil))))
-       (filter #(not (nil? %)))
-       (take 1)
-       first))
+        (map
+         (fn [n#] (try
+                    (~f n#)
+                    (catch ~execption e#
+                      nil))))
+        (#(interleave %
+                      (repeat (do (Thread/sleep 1)
+                                  nil))))
+        (filter #(not (nil? %)))
+        (take 1)
+        first))
